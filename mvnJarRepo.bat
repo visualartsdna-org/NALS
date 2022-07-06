5ea0f4f0a1bb0b3d9751b9d57fec2707be7adaf1
rspates <rspates@users.noreply.github.com>
2022-07-06 07:42:10 -0400

    Added build doc, fixed maven bat file

--- mvnJarRepo.bat
+++ mvnJarRepo.bat
@@ -1,3 +1,3 @@
 
 rem https://stackoverflow.com/questions/4955635/how-to-add-local-jar-files-to-a-maven-project
-mvn install:install-file -Dfile=target\nals-1.0.0.jar -DgroupId=org.visualartsdna.code -DartifactId=nals -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=target\nals-1.0.0.jar -DgroupId=org.visualartsdna -DartifactId=nals -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
